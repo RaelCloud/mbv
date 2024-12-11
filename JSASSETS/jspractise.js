bcd757ceb8d09e66072dcb341b694be81a7c7236
Oluyomi Juwon Temitope <holuyormijaywon@gmail.com>
2024-12-11 13:01:37 +0100

    Add files via upload

---
+++ JSASSETS/jspractise.js
@@ -0,0 +1,10 @@
+    function myproject() {
+    document.getElementById("demo").innerHTML="content is now changed! congratulations!";
+}
+
+document.getElementById("demo").innerHTML = 3 + 4;
+
+document.write(3 + 4);
+
+window.alert(3+4);
+console.log(3+4); * /
